a8dc18a67b2c4c98abfcf5280ab76837ec1c5fc1
Takemyeye <mellovan2005@gmail.com>
2024-07-19 21:53:01 +0200

    work
    dolcevita, compress belance

--- mia-app/src/components/component/pages/prijects-component/Projects.jsx
+++ mia-app/src/components/component/pages/prijects-component/Projects.jsx
@@ -8,7 +8,9 @@ const img = {
   ssPortfolio: 'img/SS-Portfolio.png',
   sushi: 'img/Sushi-Italia.jpg',  
   ss: 'img/Soul-Swap.png',
-  impactium: 'img/Impactium.jpg'
+  impactium: 'img/Impactium.jpg',
+  compress: 'img/compress.png',
+  dolcevita: 'img/DolceVita.png',
 }
 
 const Proud = () => {
@@ -40,6 +42,16 @@ const Proud = () => {
           title='Soul Swap' 
           description={translation.descriptionWork3}
           image={img.ssPortfolio} />
+        <ProjectUnit 
+          link='https://belance-compress.netlify.app/'
+          title='Belance Compress' 
+          description={translation.descriptionWork4}
+          image={img.compress} />
+        <ProjectUnit 
+          link='https://dolce-vita.netlify.app/'
+          title='Dolce Vita' 
+          description={translation.descriptionWork5}
+          image={img.dolcevita} />
       </div>
     </div>
   )
