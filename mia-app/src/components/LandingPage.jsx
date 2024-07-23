e7a08c58d973ed39a82f4f3fbddca2395263128f
Takemyeye <mellovan2005@gmail.com>
2024-07-23 03:37:15 +0200

    landing props

--- mia-app/src/components/LandingPage.jsx
+++ mia-app/src/components/LandingPage.jsx
@@ -1,27 +1,12 @@
-import React, { useContext, useMemo, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useContext } from "react";
 import Header from './Header';
-import translations from '../utils/translations';
 import ActiveContext from "./ActiveContext";
 import AuthComponent from "../private/AuthComponent"
 import './styles/Landing.scss';
-
-const video = "video/landingVideo.mp4";
+import { LandingUnit } from "./component/landingUnit/landingUnit";
 
 const Landing = () => {
   const { user } = useContext(ActiveContext);
-  const { language } = useContext(ActiveContext);
-  const translation = useMemo(() => translations[language], [language]);
-  const [fadeOut, setFadeOut] = useState(false);
-  const navigate = useNavigate();
-
-  const handleStartClick = () => {
-    setFadeOut(true);
-    setTimeout(() => {
-      navigate('/home');
-    }, 1500);
-  };
-
 
   const avatarUrl = user ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png` : null;
 
@@ -33,20 +18,8 @@ const Landing = () => {
         noJustify={true}
         hideBars={true}
         user={user && user.username}
-        avatarUrl={avatarUrl}
-      />
-      <div className={`landing-main ${fadeOut ? 'fade-out' : ''}`}>
-        <div className="landing-video">
-          <video src={video} autoPlay loop muted playsInline></video>
-          <div className="video-overlay"></div>
-          <div className="video-overlay video-overlay--top"></div>
-        </div>
-        <div className="landing-start">
-          <div className="text"> Belance </div>
-          <div className="landing-text"> {translation.innovatingSuccess} </div>
-          <div className="start" onClick={handleStartClick}> {translation.getStarted} </div>
-        </div>
-      </div>
+        avatarUrl={avatarUrl}/>
+      <LandingUnit/>
     </>
   );
 };
---
+++ mia-app/src/components/component/landingUnit/landingUnit.jsx
@@ -0,0 +1,35 @@
+import React, { useContext, useMemo, useState } from "react";
+import translations from "../../../utils/translations";
+import ActiveContext from "../../ActiveContext";
+import { useNavigate } from "react-router-dom";
+
+const video = "video/landingVideo.mp4";
+
+export function LandingUnit () {
+  const { language } = useContext(ActiveContext);
+  const translation = useMemo(() => translations[language], [language]);
+  const [fadeOut, setFadeOut] = useState(false);
+  const navigate = useNavigate();
+
+  const handleStartClick = () => {
+    setFadeOut(true);
+    setTimeout(() => {
+      navigate('/home');
+    }, 1500);
+  };
+
+  return (
+    <div className={`landing-main ${fadeOut ? 'fade-out' : ''}`}>
+        <div className="landing-video">
+          <video src={video} autoPlay loop muted playsInline></video>
+          <div className="video-overlay"></div>
+          <div className="video-overlay video-overlay--top"></div>
+        </div>
+        <div className="landing-start">
+          <div className="text"> Belance </div>
+          <div className="landing-text"> {translation.innovatingSuccess} </div>
+          <div className="start" onClick={handleStartClick}> {translation.getStarted} </div>
+        </div>
+      </div>
+  )
+}
